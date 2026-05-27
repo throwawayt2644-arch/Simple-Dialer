e400b5a3ea9f465cdafa9943326faa1d2a18723e
meilluer <mehulmathur907@gmail.com>
2026-05-27 11:31:19 +0530

    fix compilation issue: place RtlViewPager.java in src/main/kotlin

---
+++ app/src/main/kotlin/com/duolingo/open/rtlviewpager/RtlViewPager.java
@@ -0,0 +1,15 @@
+package com.duolingo.open.rtlviewpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import androidx.viewpager.widget.ViewPager;
+
+public class RtlViewPager extends ViewPager {
+    public RtlViewPager(Context context) {
+        super(context);
+    }
+
+    public RtlViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
